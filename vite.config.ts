83dddf23640be8d78520c87f6e72f27276fab086
racarmona89947 <racarmona09@gmail.com>
2026-09-14 08:59:49 -0500

    chore: protect local env and stabilize test runner

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,7 @@ export default defineConfig({
     setupFiles: './src/test/setup.ts',
     css: true,
     include: ['src/test/**/*.test.{ts,tsx}'],
+    pool: 'threads',
+    maxWorkers: 1,
   },
 })
